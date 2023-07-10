022976992033c90900061c55ba1da54c95db17e2
Waldísio Araújo <waldisius@gmail.com>
2023-07-10 14:35:35 -0300

    Criadas páginas provisórias de Login e Logout e bloqueando algumas páginas contra acesso por usuário não logado

--- filme/urls.py
+++ filme/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import Homepage, Homefilmes, Detalhesfilme, Pesquisafilme
+from django.contrib.auth import views as auth_view
 
 app_name = 'filme'
 
@@ -9,4 +10,6 @@ urlpatterns = [
     path('filmes/', Homefilmes.as_view(), name='homefilmes'),
     path('filmes/<int:pk>', Detalhesfilme.as_view(), name='detalhesfilme'),
     path('pesquisa/', Pesquisafilme.as_view(), name='pesquisafilme'),
+    path('login/', auth_view.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_view.LogoutView.as_view(template_name='logout.html'), name='logout'),
 ]
--- filme/views.py
+++ filme/views.py
@@ -1,18 +1,19 @@
 from django.shortcuts import render
 from .models import Filme
 from django.views.generic import TemplateView, ListView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 class Homepage(TemplateView):
     template_name = 'homepage.html'
 
 
-class Homefilmes(ListView):
+class Homefilmes(LoginRequiredMixin, ListView):
     template_name = 'homefilmes.html'
     model = Filme
 
 
-class Detalhesfilme(DetailView):
+class Detalhesfilme(LoginRequiredMixin, DetailView):
     template_name = 'detalhesfilme.html'
     model = Filme
 
@@ -35,7 +36,7 @@ class Detalhesfilme(DetailView):
         return context
 
 
-class Pesquisafilme(ListView):
+class Pesquisafilme(LoginRequiredMixin, ListView):
     template_name = 'pesquisa.html'
     model = Filme
 
