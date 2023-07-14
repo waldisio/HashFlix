d99f02afacdac166347577a29e7d9935e4650a13
Waldísio Araújo <waldisius@gmail.com>
2023-07-14 13:09:20 -0300

    Descomentada função ready()

--- filme/apps.py
+++ filme/apps.py
@@ -5,7 +5,7 @@ class FilmeConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'filme'
 
-"""    def ready(self):
+    def ready(self):
         from .models import Usuario
         import os
 
@@ -15,4 +15,3 @@ class FilmeConfig(AppConfig):
         usuarios = Usuario.objects.filter(email=email)
         if not usuarios:
             Usuario.objects.create_superuser(username='admin', email=email, password=senha, is_active=True, is_staff=True)
-"""
